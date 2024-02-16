8c5e26486eef3001bf73318dd9b106c89e20ac58
melekpro <melekpro6@gmail.com>
2024-02-16 18:25:28 +0100

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -8,5 +8,5 @@
 LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),TECNO-BA2)
-include $(call all-subdir-makefiles,$(LOCAL_PATH))
+include $(call all-subdir-makefiles,$(device/tecno/TECNO-BA2))
 endif
